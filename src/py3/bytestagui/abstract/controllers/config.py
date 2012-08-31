373795febe885ad72a5eb27c5b7bf6b0e5e718f7
Christopher Foo <chris.foo@gmail.com>
2012-08-30 20:43:32 -0400

    Updates ConfigController with convenience functions.

--- src/py3/bytestagui/abstract/controllers/config.py
+++ src/py3/bytestagui/abstract/controllers/config.py
@@ -3,18 +3,25 @@
 # Copyright © 2012 Christopher Foo <chris.foo@gmail.com>.
 # Licensed under GNU GPLv3. See COPYING.txt for details.
 from bytestag import basedir
+from bytestag.events import Observer
 from bytestag.files import file_overwriter
 from bytestag.keys import KeyBytes
 from bytestagui.abstract.controllers.base import BaseController
 from collections import OrderedDict
 import configparser
+import logging
 import os.path
+
+
+_logger = logging.getLogger(__name__)
 
 
 class ConfigController(BaseController):
     def __init__(self, application):
         BaseController.__init__(self, application)
         config_parser = configparser.ConfigParser()
+        default_share_dir = os.path.expanduser(os.path.join('~', 'Bytestag'))
+
         config_parser.read_dict(OrderedDict([
             ('network', OrderedDict([
                 ('host', '0.0.0.0'),
@@ -27,6 +34,9 @@ class ConfigController(BaseController):
             ])),
             ('shared_files', OrderedDict([
             ])),
+            ('sharing', OrderedDict([
+                ('default_share_dir', default_share_dir),
+            ])),
         ]))
 
         filename = os.path.join(basedir.config_dir, 'bytestag.conf')
@@ -34,13 +44,29 @@ class ConfigController(BaseController):
 
         self._config_parser = config_parser
         self._filename = filename
+        self._observer = Observer()
 
     @property
     def config_parser(self):
         return self._config_parser
+
+    @property
+    def observer(self):
+        return self._observer
 
     def save(self):
         os.makedirs(os.path.dirname(self._filename), mode=0o777, exist_ok=True)
 
         with file_overwriter(self._filename, flags='w') as f:
             self._config_parser.write(f)
+
+    def set(self, section, option, value):
+        self._config_parser[section][option] = value
+
+        self._observer(section, option, value)
+
+    def get(self, section, option, as_bool=False):
+        if as_bool:
+            return self._config_parser.getboolean(section, option)
+
+        return self._config_parser[section][option]
